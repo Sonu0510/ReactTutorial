39d77fd83d085652dbe22ccea3822d3bfb7b0d43
YugendraRay <ray.yuge@gmail.com>
2022-08-02 02:12:24 +1000

    added bootstrap in react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-let name = "Sonu";
 function App() {
   return (
     <>   
-      <nav>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Portfolio</li>
-      </nav>
-      <img src="" alt="" />
-      <div className="container">
-        <h1>Hello {name} </h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim officiis laborum aliquam quisquam quis atque veritatis ullam totam deserunt. Veritatis sequi voluptatibus sunt dolorum. Eveniet sequi amet laboriosam vel id!
-        </p>
-      </div>
+     <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">Text Utils</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/">About</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
 
-      </>
+    </>
   );
 }
 
