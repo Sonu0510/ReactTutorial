53dfb4362d0f283f279d02c097316475584986d8
YugendraRay <ray.yuge@gmail.com>
2022-08-02 01:35:42 +1000

    First react Folder and app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+let name = "Sonu";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>   
+      <nav>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Portfolio</li>
+      </nav>
+      <img src="" alt="" />
+      <div className="container">
+        <h1>Hello {name} </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim officiis laborum aliquam quisquam quis atque veritatis ullam totam deserunt. Veritatis sequi voluptatibus sunt dolorum. Eveniet sequi amet laboriosam vel id!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+
+      </>
   );
 }
 
