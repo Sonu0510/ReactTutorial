14729b26faec335a3c6a31750b27878c172551a5
YugendraRay <ray.yuge@gmail.com>
2022-08-04 13:16:37 +1000

    Navbar darkmode toggle

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 import Navbar from './components/Navbar.js';
 import Form from './components/Form.js';
 import About from './components/About';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [mode, setMode] = useState("light");
+
+  const toggleMode = ()=> {
+    if(mode==="dark"){
+      setMode("light");
+    }else{
+      setMode("dark");
+    }
+  }
+
   return (
     <>
 
-      <Navbar title="TextUtils"/>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
       <About/>
       <Form heading="Enter text here to modify"/>
 
